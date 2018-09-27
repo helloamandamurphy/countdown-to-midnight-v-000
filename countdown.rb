8143cb88cbb76ebce1756d48242d48e96e8d0dd0
Amanda Murphy <helloamandamurphy@gmail.com>
2018-09-27 16:28:50 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,10 @@
-#write your code here
-seconds = 12
-
 def countdown(seconds)
-  while seconds > 0
-    puts "#{seconds} SECOND(S)!"
-    seconds -= 1 
-    when = 0 
-      "HAPPY NEW YEAR!"
-    end
-  end
+  seconds = 12
+  until counter = 0 
+  puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end 
 end
+  
 
 countdown(seconds)
